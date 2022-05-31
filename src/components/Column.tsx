950585b006b5b79ac262aa20b3df8021834dccce
00014973 <fozilzayniddinov7777@gmail.com>
2022-06-01 00:33:04 +0500

    setup contex, adding tasks and lists

--- src/components/Column.tsx
+++ src/components/Column.tsx
@@ -1,16 +1,24 @@
 import { ColumnContainer, ColumnTitle } from "../styles"
 import { AddNewItem } from "./AddNewItem"
+import { useAppState } from '../state/AppStateContext'
+import { Card } from './Card'
 
 type ColumnProps = {
     text: string,
-    children?: React.ReactNode
+    id: string,
 }
 
-export const Column: React.FC<ColumnProps> = ({ text, children }) => {
+export const Column: React.FC<ColumnProps> = ({ text, id }) => {
+
+    const { getTasksByListId } = useAppState()
+    const tasks = getTasksByListId(id)
+
     return (
         <ColumnContainer>
             <ColumnTitle>{text}</ColumnTitle>
-            {children}
+            {tasks.map(task => (
+                <Card text={task.text} key={task.id} id={task.id} />
+            ))}
             <AddNewItem
                 toggleButtonText="+ Add another task"
                 onAdd={console.log}
